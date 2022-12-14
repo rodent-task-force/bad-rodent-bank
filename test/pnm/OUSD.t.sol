91bd3ae9d84e14d70c540892dbfe0082297390cd
Daniel Von Fange <daniel@leancoder.com>
2022-12-14 10:11:55 -0500

    Test with OUSD

--- test/pnm/OUSD.t.sol
+++ test/pnm/OUSD.t.sol
@@ -4,8 +4,9 @@ import "../../src/OUSD/contracts/token/OUSD.sol";
 contract OUSDFuzzTest is PTest {
     OUSD ousd;
     address agent;
-    address payable depositor;
-    address payable vault;
+    address alice = address(0x0100);
+    address bob = address(0x0101);
+    address vault = address(0x0201);
 
     function setUp() external {
         agent = getAgent();
@@ -13,22 +14,27 @@ contract OUSDFuzzTest is PTest {
         ousd = new OUSD();
         ousd.initialize("OUSD", "OUSD", address(vault));
 
-        vm.deal(depositor, 100);
-        // vm.prank(depositor);
-
-        vm.deal(vault, 100);
-        // vm.prank(vault);
+        vm.deal(payable(alice), 1e18);
+        vm.deal(payable(bob), 1e18);
+        vm.deal(payable(vault), 1e18);
+        
     }
 
     function invariantBalanceLessThanTotalSupply() public {
         assertLe(
-            ousd.balanceOf(address(depositor)),
+            ousd.balanceOf(address(alice)),
             ousd.totalSupply()
             );
     }
 
-    function attack() public {
-	// vm.prank(agent);
-	// bank.withdrawFrom(agent, 2);
+    function testMint() public {
+        vm.prank(vault);
+    	ousd.mint(alice, 100e18);
+        
+        vm.prank(vault);
+        ousd.changeSupply(250e18);
+
+        vm.prank(vault);
+        ousd.mint(alice, 1);
     }
 }
